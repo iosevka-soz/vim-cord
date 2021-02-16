6590224f595221b9de15508d9760c000a83c0d3b
iosevka <iosevka@protonmail.com>
2021-02-16 08:38:54 +0000

    Added support for other editors as other discord apps, proper disabling and proper handling of disord not connecting, added reconnect command

--- rplugin/python3/vim-cord.py
+++ rplugin/python3/vim-cord.py
@@ -7,70 +7,79 @@ import nest_asyncio
 from time import time as nowTime
 nest_asyncio.apply()
 
-defaultVimAppId = "746988158629052416"
-
 @pynvim.plugin
 class vimCord(object):
 
-    __disabled           = False
+    openBuffers = []
+    disabled    = False
+    editorName  = True
+    vim         = None
+    discord     = None
+    bufState    = {}
+    rpcState    = {}
+    config      = {}
 
-    __vim                = None
-    __discord            = None
-    __currentBufferState = None
-    __currentRPCState    = None
-    __config             = None
+    def getVariable(self, value: str):
+        try:
+            return self.vim.eval(value)
+        except:
+            return None
 
-    def getBufferState(self) -> dict:
-        bufferState = {}
-        bufferState["filetype"]   = self.getVariable("&filetype")
-        bufferState["filename"]   = self.getVariable("expand('%:t')")
-        bufferState["modifiable"] = self.getVariableBool("&modifiable")
+    def parseBufState(self) -> dict:
+        self.bufState["ftype"] = self.getVariable("&filetype")
+        self.bufState["fname"] = self.getVariable("expand('%:t')")
+        self.bufState["isMod"] = self.getVariable("&modifiable")
+        self.bufState["modPrefix"] = "Editing" if self.bufState["isMod"] else "Reading"
+        return self.bufState
 
-        self.__currentBufferState = bufferState
-        return bufferState
+    def parseEditorName(self, override : Optional[str]) -> str:
+        def getVimOrNvimFromEnv() -> str:
+            return "neovim" if self.vim.command_output("if has('nvim') | echo 1 | endif") else "vim"
+
+        if not override:
+            return getVimOrNvimFromEnv()
+
+        override = override.lower()
+        editorOptions = [ 'vi', 'vim', 'gvim', 'macvim', 'neovim' ]
+
+        if override in editorOptions:
+            return override
+        else:
+            self.vim.err_write(f'Invalid editor name!') #TODO: add editor names on error output
+            return getVimOrNvimFromEnv()
+
+    def parseAppId(self, override, editorName) -> str:
+        return override if override else  {
+                                            "vi"     : "809799093873278976",
+                                            "vim"    : "746988158629052416",
+                                            "neovim" : "746627198458527816",
+                                            "gvim"   : "809799169308098668",
+                                            "macvim" : "809799485327671317"
+                                          }[editorName]
 
     def parseConfig(self) -> dict:
-        config = {}
-        config["discord_client_id"] = self.getVariable("g:vim_cord_discord_client_id")
-        config["discord_retry"]     = self.getVariableBool("g:vim_cord_discord_retry")
-        config["discord_max_retry"] = self.getVariable("g:vim_cord_discord_max_retry")
-        return config
+        prefix = "g:vim_cord"
+        self.config["editor"] = self.parseEditorName(self.getVariable(f'{prefix}_editor_override'))
+        self.config["discord_app_id"] = self.parseAppId(self.getVariable(f'{prefix}_app_id'), self.config["editor"])
+        self.config["discord_retry"]     = self.getVariable(f'{prefix}_discord_retry')
+        self.config["discord_max_retry"] = self.getVariable(f'{prefix}_max_retry')
+        self.config["track_modifiable"]  = self.getVariable(f'{prefix}_track_modifiable')
+        return self.config
 
-    def getVariable(self, value: str) -> Optional[str]:
-        try:
-            return self.__vim.eval(value)
-        except:
-            return None
+    def buildRPCUpdate(self, restartTimer : bool) -> dict:
+        self.rpcState["details"]     = f'{self.bufState["modPrefix"]}: {self.bufState["fname"]}'
+        self.rpcState["start"]       = nowTime() if restartTimer else None #Restart timer?
+        self.rpcState["small_image"] = self.config["editor"] 
+        self.rpcState["large_image"] = self.bufState["ftype"] if self.bufState["ftype"] != "" else None
+        return self.rpcState
 
-    def getVariableBool(self, value: str) -> Optional[bool]:
-        try:
-            variableValue = self.__vim.eval(value)
-            return 1 if variableValue == 1 else 0 if variableValue == 0 else None
-        except:
-            return None
+    def discordConnect(self, retry : Optional[bool], maxRetries : Optional[int]) -> Optional[Presence]:
+        if not retry:
+            retry = False
+        if not maxRetries:
+            maxRetries = 3
 
-    def getFilenamePrefix(self, modifiable : bool) -> str:
-        return "Editing" if modifiable else "Reading"
-
-    def die(self):
-        self.__disabled = True
-
-    def buildRPCUpdate(self, bufferState : dict, restartTimer : bool) -> dict:
-        RPCUpdate = {}
-
-        RPCUpdate["details"]     = f'{self.getFilenamePrefix(bufferState["modifiable"])}: {bufferState["filename"]}'
-        RPCUpdate["start"]       = nowTime() if restartTimer else None #Restart timer?
-        RPCUpdate["small_image"] = "vim" #For now just use vim image TODO: Dynamically update based on vim version
-        RPCUpdate["large_image"] = bufferState["filetype"] if bufferState["filetype"] != "" else None
-
-        self.__currentRPCState = RPCUpdate
-        return RPCUpdate
-
-    def discordConnect(self, retry : bool, maxRetries : int, cid : str) -> Presence:
-        if cid == None:
-            cid = defaultVimAppId
-
-        rpc = Presence(client_id = cid)
+        rpc = Presence(client_id = self.config["discord_app_id"])
         def try_connect() -> bool:
             try:
                 rpc.connect()
@@ -78,53 +87,62 @@ class vimCord(object):
             except:
                 return False
 
-        success : bool = try_connect()
-        if success:
+        if try_connect():
             return rpc
         elif retry:
-            retryCount : int = 0
+            retryCount = 0
             while not try_connect():
-                self.__vim.err_write(f'Discord connection failed, retrying...\n')
-                if retryCount >= (1 if maxRetries == None else maxRetries):
-                    self.__vim.err_write(f'Discord connection max retries exceeded, exiting\n')
-                    self.die()
+                self.vim.err_write(f'Discord connection failed, retrying {maxRetries - retryCount} more times...\n')
+                if retryCount >= maxRetries:
+                    self.vim.err_write(f'Discord connection max retries exceeded, exiting\n')
+                    self.disabled = True
                     return None
                 retryCount += 1
+            self.disabled = False
             return rpc
-
+    
     #MUST be synchronous or handler will not be registered
     @pynvim.autocmd('VimLeave', allow_nested=False, sync=True)
     def onVimLeave(self):
-        if not self.__disabled:
-            self.__discord.clear()
-            #self.__discord.get_event_loop().stop() #Maybe don't stop this loop?
-            self.__discord.close()
+        if self.disabled:
+            pass
+        self.discord.clear()
+        self.discord.close()
 
     @pynvim.autocmd('BufEnter', allow_nested=True, sync=False)
-    def onBufferEnter(self):
-        if not self.__disabled:
-            bufferState : dict = self.getBufferState()
-            #TODO: Memoization algorithm for open buffers to cache timers?
-            RPCUpdate   : dict = self.buildRPCUpdate(bufferState, restartTimer=True) #Always restart timer when new buffer is entered
-            self.__discord.update(**RPCUpdate)
+    def onBufEnter(self):
+        if self.disabled:
+            pass
+        self.parseBufState()
+        if self.discord != None: 
+            self.discord.update(**self.buildRPCUpdate(restartTimer=True))
 
-    #This apparently only runs after buffer is written to disk TODO: Find way to run async?
     @pynvim.autocmd('BufModifiedSet', allow_nested=True, sync=False, eval="&modifiable")
-    def onBufferModifiedSet(self, newModifiable : int):
-        if not self.__disabled:
-            if self.__currentBufferState != None and self.__currentBufferState["modifiable"] != newModifiable:
-                self.__currentRPCState["details"] = f'{self.getFilenamePrefix(newModifiable)}: {self.__currentBufferState["filename"]}'
-                self.__discord.update(**self.__currentRPCState)
-                self.__currentBufferState["modifiable"] = newModifiable
+    def onBufModifiedSet(self, newModifiable : int):
+        if self.disabled or not self.config["track_modifiable"]:
+            pass
+        if self.bufState["isMod"] != newModifiable:
+            self.bufState["isMod"] = newModifiable
+            self.bufState["modPrefix"] = "Editing" if newModifiable else "Reading"
+            self.rpcState["details"] = f'{self.bufState["modPrefix"]}: {self.bufState["fname"]}'
+            self.discord.update(**self.rpcState)
+
+    @pynvim.command('VimCordReconnect', 0, sync=False, eval="g:vim_cord_discord_max_retry")
+    def VimCordRetry(self, maxRetries : int):
+        if self.disabled:
+            self.vim.out_write("Vim-cord is disabled\n")
+            pass
+        self.discord.clear()
+        self.discord.close()
+        if not self.discordConnect(True, maxRetries):
+            self.disabled = False
 
     def __init__(self, vim):
         #Set this variable in your vimrc/init.vim to disable vim-cord
-        isLoaded = self.getVariable("g:loaded_vim_cord")
-        if isLoaded != None:
-            self.die()
+        if self.getVariable("g:loaded_vim_cord") != None:
+            self.disabled = True
 
-        self.__vim     = vim
-        self.__config  = self.parseConfig()
-        self.__discord = self.discordConnect(self.__config["discord_retry"],
-                                             self.__config["discord_max_retry"],
-                                             self.__config["discord_client_id"])
+        self.vim     = vim
+        self.config  = self.parseConfig()
+        self.discord = self.discordConnect(self.config["discord_retry"], self.config["discord_max_retry"])
+
